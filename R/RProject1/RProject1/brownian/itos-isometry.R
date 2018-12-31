9b5fe1d02cf6dd538b7e8b16e624ad45f6e7296d
miahal <alomgirmiah@gmail.com>
2018-12-31 14:16:54 -0500

    fix its isometry

--- R/RProject1/RProject1/brownian/itos-isometry.R
+++ R/RProject1/RProject1/brownian/itos-isometry.R
@@ -1,55 +1,64 @@
+G <- function(t) {
+    return (t ^ 2 + t);
+}
+
+H <- function(t) {
+    return(t ^ 3 + t);
+}
+
 simItosIsemetry <- function(T, N) {
     # tim step size
     dt <- T / N;
 
+    t <- rep(0, N);
+
     XG <- rnorm(N, 0, 1);
     XH <- rnorm(N, 0, 1);
 
-    dWG <- rep(0, N);
-    dWH <- rep(0, N);
+    dW <- rep(0, N);
 
-    GdWG <- rep(0, N);
-    HdWH <- rep(0, N);
+    GdW <- rep(0, N);
+    HdW <- rep(0, N);
     GHdT <- rep(0, N);
 
     for (i in 2:N) {
-        t <- i * dt;
+        t[i] <- i * dt;
 
-        dWG[i] <- sqrt(dt) * XG[i];
-        dWH[i] <- sqrt(dt) * XH[i];
+        dW[i] <- sqrt(dt) * XH[i];
 
-        GdWG[i] <- (t ^ 2 + sin(t)) * dWG[i];
-        HdWH[i] <- (t ^ 3 + cos(t)) * dWH[i];
-        GHdT[i] <- (t ^ 2 + sin(t)) * (t ^ 3 + cos(t)) * dt;
+        GdW[i] <- G(t[i]) * dW[i];
+        HdW[i] <- H(t[i]) * dW[i];
+        GHdT[i] <- G(t[i]) * H(t[i]) * dt;
     }
 
-    return(list(GdWG = GdWG, HdWH = HdWH, GHdT = GHdT));
+    return(list(GdW = GdW, HdW = HdW, GHdT = GHdT));
 }
 
-T <- 10;
-N <- 100;
-dt <- T / N;
+T <- 1;
+N <- 1000;
 
 res <- simItosIsemetry(T, N);
 
 # generate many paths
-M <- 100;
-IGdWG <- rep(0, M);
-IHdWH <- rep(0, M);
+M <- 1000;
+IGdW <- rep(0, M);
+IHdW <- rep(0, M);
 IGHdT <- rep(0, M);
 
 for (i in 1:M) {
     res <- simItosIsemetry(T, N);
-    IGdWG[i] <- sum(res$GdWG);
-    IHdWH[i] <- sum(res$HdWH);
+    IGdW[i] <- sum(res$GdW);
+    IHdW[i] <- sum(res$HdW);
     IGHdT[i] <- sum(res$GHdT);
 }
 
 
 par(mfrow = c(3, 1))
-plot(seq(1:N), IGdWG, xlab = "time", ylab = "IGdWG", col = "red", type = "l");
-plot(seq(1:N), IHdWH, xlab = "time", ylab = "IHdWH", col = "green", type = "l");
-plot(seq(1:N), IGHdT, xlab = "time", ylab = "IGHdT", col = "purple", type = "l");
+hist(IGdW, xlab = "time", ylab = "IGdW", col = "red");
+hist(IHdW, xlab = "time", ylab = "IHdW", col = "green");
+hist(IGHdT, xlab = "time", ylab = "IGHdT", col = "purple");
 
-mean(IGdWG * IHdWH)
+mean(IGdW)
+mean(IHdW)
+mean(IGdW * IHdW)
 mean(IGHdT)
