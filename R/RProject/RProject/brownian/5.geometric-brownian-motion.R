d73493b3089a476380e109fd97a14a24b63a34be
miahal <alomgirmiah@gmail.com>
2019-01-02 14:36:32 -0500

    fix brownian motion

--- R/RProject/RProject/brownian/5.geometric-brownian-motion.R
+++ R/RProject/RProject/brownian/5.geometric-brownian-motion.R
@@ -1,35 +1,43 @@
-# SDE -> dX = sigma * X * dW 
+# SDE -> dX / X = sigma * dW 
 # Solution -> X = exp ( -0.5 * (sigma ^ 2) * 2 + sigma + W)
 # mean(X) = 1
 # variance(X) = exp( sigma ^ 2 * t )
 # p = 0.5
+
 
 T <- 10;
 N <- 1000;
 dt <- T / N;
 sigma <- 0.2
 
-# generate many paths
+# generate many paths for simulation and closed form
 M <- 1000;
 W <- rep(0, M);
-GB <- rep(0, M);
+XS <- rep(0, M);
+XC <- rep(0, M);
 
 for (i in 1:M) {
-    dW <- rnorm(N, 0, sqrt(dt));
+    dW <- rnorm(N, mean = 0, sd = sqrt(dt));
     W[i] <- sum(dW);
+    X <- rep(0, M);
+
+    X[1] <- 1;
+    for (j in 2:N) {
+        X[j] <- X[j - 1] + X[j - 1] * (sigma * dW[j - 1]);
+    }
+
+    XS[i] = X[N];
+    XC[i] = exp((- 0.5 * (sigma ^ 2)) * T + sigma * W[i]);
 }
 
-GB <- exp(-0.5 * (sigma ^ 2) * T + sigma + W)
-
 par(mfrow = c(2, 1))
-hist(W, col = "green")
-hist(GB, col = "blue")
+hist(XS, col = "green", breaks = 100)
+hist(XC, col = "blue", breaks = 100)
 
-sprintf("Mean=%f Variance=%f", 1, exp(sigma ^ 2 * T))
-sprintf("Mean=%f Variance=%f", mean(GB), sd(GB) ^ 2)
+sprintf("Mean=%f Variance=%f", mean(XS), sd(XS) ^ 2)
+sprintf("Mean=%f Variance=%f", mean(XC), sd(XC) ^ 2)
 
-
-# SDE -> dX = mu * X * dt + sigma * X * dW 
+# SDE -> dX / X = mu * dt + sigma * dW 
 # Solution -> X = X0 * exp ((mu - 0.5 * sigma ^ 2) * T + sigma + W)
 # mean(X) = mu
 # variance(X) = sigma ^ 2 ????
@@ -42,7 +50,7 @@ sigma <- 0.2
 mu <- 0.2
 
 # generate many paths for simulation and closed form
-M <- 10;
+M <- 1000;
 W <- rep(0, M);
 XS <- rep(0, M);
 XC <- rep(0, M);
@@ -58,42 +66,53 @@ for (i in 1:M) {
     }
 
     XS[i] = X[N];
-    XC[i] = exp((mu - 0.5 * (sigma ^ 2)) * T + sigma + W[i]);
+    XC[i] = exp((mu - 0.5 * (sigma ^ 2)) * T + sigma * W[i]);
 }
 
 par(mfrow = c(2, 1))
-hist(XS, col = "green")
-hist(XC, col = "blue")
+hist(XS, col = "green", breaks = 100)
+hist(XC, col = "blue", breaks = 100)
 
 sprintf("Mean=%f Variance=%f", mean(XS), sd(XS) ^ 2)
 sprintf("Mean=%f Variance=%f", mean(XC), sd(XC) ^ 2)
 
 
+#Geometric Brownian Motion
+maturity <- 15
+simulation.length <- 10001
+dt <- maturity / (simulation.length - 1)
 
-##inputs:
-alpha = 0;
-sigma = 1;
-T = 1;
-n = 2 ^ (12);
-X0 = 0.1;
-#############Generate 1 trajectory
-dt = T / n
-t = seq(0, T, by = dt)
-x = c(X0, alpha * dt + sigma * sqrt(dt) * rnorm(n, mean = 0, sd = 1))
-Xt = cumsum(x)
-par(mfrow = c(1, 1))
-plot(t, Xt, type = 'l', xlab = "time")
-sprintf("Mean=%f Variance=%f", mean(Xt), sd(Xt) ^ 2)
+timeline <- seq(0, maturity, dt)
+
+S0 <- 1
+r <- 0.05
+mu <- 0.1
+mu0 <- 0.2
+sigma <- 0.2
+sigma0 <- 0.375
+
+f <- g <- g0 <- h <- h0 <- rep(0, times = simulation.length)
+g0[1] <- h0[1] <- g[1] <- h[1] <- S0
+
+for (i in 2:simulation.length) {
+    f[i] <- f[i - 1] + sqrt(dt) * rnorm(1)
+    g[i] <- g[1] * exp((mu - (sigma ^ 2) / 2) * (i - 1) * dt + sigma * f[i])
+    g0[i] <- g0[1] * exp(mu * (i - 1) * dt)
+    h[i] <- h[1] * exp((mu0 - (sigma0 ^ 2) / 2) * (i - 1) * dt + sigma0 * f[i])
+    h0[i] <- h0[1] * exp(mu0 * (i - 1) * dt)
+}
+
+o_range <- range(f, g, g0, h, h0)
+
+plot(timeline, f, ylim = o_range, type = "l", col = "black")
+lines(timeline, g0, col = "red")
+lines(timeline, g, col = "green")
+lines(timeline, h, col = "blue")
+lines(timeline, h0, col = "orange")
+
+title(main = "Geometric Brownian Motion trajectories", col.main = "red", font.main = 4)
 
 
 
-set.seed(12345)
-f0 <- 102
-mu <- (0.05)
-sigma <- 0.08
-T <- 0.5
-t <- seq(1 / 365, T
-f <- numeric(n)
-f <- f0 * exp(cumsum((mu - sigma * sigma / 2) * T / n + sigma * sqrt(T / n) * rnorm(n)))
-diff <- f - f0
-plot(t, f, type = "l")
+legend(1, o_range[2], c("mu = 0.2,  sigma = 0.375", "mu = 0.1,  sigma = 0.2", "Brownian motion"), cex = 0.8,
+   col = c("blue", "green", "black"), pch = 1, lty = 1);
